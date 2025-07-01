4927883ed8155d38f694d0a31e6342c7239e6cb8
Azer Agazade <azer.agazade05@gmail.com>
2025-07-01 18:32:53 +0400

    fixed dto

--- proxy/src/main/java/ru/example/proxy/dto/ReceiveCommand.java
+++ proxy/src/main/java/ru/example/proxy/dto/ReceiveCommand.java
@@ -1,16 +1,11 @@
 package ru.example.proxy.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ReceiveCommand extends CommandMessage {
-    {
+
+    public ReceiveCommand() {
         this.command = "RECEIVE";
     }
 }
